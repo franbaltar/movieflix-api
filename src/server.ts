f6407d695452e7b95f1694488ee035ae3d6e2be5
Francisco Baltar <fran.bmontana@gmail.com>
2026-07-03 12:11:34 -0300

    adicionada ordenação, gênero e idioma do filme

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,18 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.get("/movies", async (_, res) => {
-   const movies = await prisma.movie.findMany();
-   res.json(movies);
+  const movies = await prisma.movie.findMany({
+    orderBy: {
+      title: "asc",
+    },
+    include: {
+      genres: true,
+      languages: true,
+    },
+  });
+  res.json(movies);
 });
 
 app.listen(port, () => {
-   console.log(`Servidor em execução em http://localhost:${port}`);
+  console.log(`Servidor em execução em http://localhost:${port}`);
 });
